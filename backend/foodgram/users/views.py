a18ff3cd07b756c14af57d99b2f358c7e26510f8
LunarBirdMYT <pav.mgk50@mail.ru>
2023-03-13 15:45:01 +0300

    Правки вьюх юзверей

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import exceptions, status
@@ -8,15 +7,12 @@ from rest_framework.permissions import (IsAuthenticated,
 from rest_framework.response import Response
 
 from .models import Subscription
-from .pagination import CustomPageNumberPagination
 from .serializers import SubscriptionSerializer
-
-User = get_user_model()
+from .models import User
 
 
 class CustomUserViewSet(UserViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    pagination_class = CustomPageNumberPagination
 
     @action(
         detail=False,
