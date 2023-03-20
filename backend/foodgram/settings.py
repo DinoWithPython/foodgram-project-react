474884d4a5c9a91ed9fedfa587a1d7041a8125c0
LunarBirdMYT <pav.mgk50@yandex.ru>
2023-03-20 23:00:01 +0300

    и еще раз ворк

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,10 +7,6 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
     "SECRET_KEY", default="r=w+3lf%txx&%e9z&y@xmar!k#d5%*bsrf6(6x19fy=&5#01wj"
