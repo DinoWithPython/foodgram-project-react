43e5772817b8760c567116bfa51a72679e6d738b
LunarBirdMYT <pav.mgk50@yandex.ru>
2023-03-20 23:12:54 +0300

    secret_key in venv

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,6 +7,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
     "SECRET_KEY", default="r=w+3lf%txx&%e9z&y@xmar!k#d5%*bsrf6(6x19fy=&5#01wj"
