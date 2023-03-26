efaa5fa3f01e8b8d9330f7420d500c4f5ea46025
LunarBirdMYT <pav.mgk50@yandex.ru>
2023-03-26 14:17:11 +0300

    Почему-то сохранение миграций не прошло.

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -39,8 +39,7 @@ class Migration(migrations.Migration):
                     "is_superuser",
                     models.BooleanField(
                         default=False,
-                        help_text=("Designates that this user has all "
-                        "permissions without explicitly assigning them."),
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",
                         verbose_name="superuser status",
                     ),
                 ),
@@ -48,8 +47,7 @@ class Migration(migrations.Migration):
                     "is_staff",
                     models.BooleanField(
                         default=False,
-                        help_text=("Designates whether the user can log"
-                        " into this admin site."),
+                        help_text="Designates whether the user can log into this admin site.",
                         verbose_name="staff status",
                     ),
                 ),
@@ -57,8 +55,7 @@ class Migration(migrations.Migration):
                     "is_active",
                     models.BooleanField(
                         default=True,
-                        help_text=("Designates whether this user should be treated as"
-                        " active. Unselect this instead of deleting accounts."),
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
                         verbose_name="active",
                     ),
                 ),
@@ -85,8 +82,7 @@ class Migration(migrations.Migration):
                     "groups",
                     models.ManyToManyField(
                         blank=True,
-                        help_text=("The groups this user belongs to. A user will get all permissions"
-                        " granted to each of their groups."),
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.Group",
