8e5fa47b0219f664590952b492a0d41c46f25991
LunarBirdMYT <pav.mgk50@yandex.ru>
2023-03-29 21:20:14 +0300

    Указываю конкретные поля...

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -254,6 +254,10 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
+        fields = (
+            'id', 'tags', 'author', 'ingredients', 'is_favorited',
+            'is_in_shopping_cart', 'name', 'image', 'text', 'cooking_time'
+        )
         exclude = ("pub_date",)
 
 
